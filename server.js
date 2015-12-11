df00b6fd13c93d23dec6fbbe3638f93215864510
allen wes <allen.wes.g@gmail.com>
2015-12-11 15:39:55 -0600

    added routes for all todos and individual todos

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [
+	{
+		id: 1,
+		description: "meet mom for lunch",
+		completed: false
+	},
+	{
+		id: 2,
+		description: "go to market",
+		completed: false
+	},
+	{
+		id: 3,
+		description: "walk the cat",
+		completed: false
+	}
+];
 
 app.get("/", function(req,res){
 	res.send("todo is up");
+});
+
+app.get("/todos", function(req, res){
+	res.json(todos);
+});
+
+app.get("/todos/:id", function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var flag = true;
+	for(var i = 0; i < todos.length; i++){
+		if(todos[i].id === todoId){
+			flag = false;
+			res.json(todos[i]);
+			break;
+		}
+	}
+	if(flag){
+		res.status(404).send("Todo Item could not be found");
+	}
 });
 
 app.listen(PORT, function(){
